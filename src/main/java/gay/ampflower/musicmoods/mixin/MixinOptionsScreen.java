a2b8ae89f73d4b2fcb9bc9d83dcf98d5731f40bc
Ampflower 🌺 <git@ampflower.gay>
2025-10-19 08:35:27 -0700

    feat: Redirect Music & Sounds button to Music Moods config

---
+++ src/main/java/gay/ampflower/musicmoods/mixin/MixinOptionsScreen.java
@@ -0,0 +1,59 @@
+package gay.ampflower.musicmoods.mixin;
+
+import gay.ampflower.musicmoods.Config;
+import gay.ampflower.musicmoods.client.ConfigurationScreen;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.screens.Screen;
+#if MC_1_20_5_OR_OLDER
+import net.minecraft.client.gui.screens.OptionsScreen;
+#else
+import net.minecraft.client.gui.screens.options.OptionsScreen;
+#endif
+import net.minecraft.network.chat.Component;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+#if MC_1_19_OR_OLDER
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+#endif
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/**
+ * @author Ampflower
+ * @since 0.7
+ **/
+@Mixin(OptionsScreen.class)
+@Environment(EnvType.CLIENT)
+public abstract class MixinOptionsScreen extends Screen {
+	protected MixinOptionsScreen(final Component component) {
+		super(component);
+	}
+
+	#if MC_1_19_OR_OLDER
+	@ModifyArg(
+		method = "method_19829",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/client/Minecraft;setScreen(Lnet/minecraft/client/gui/screens/Screen;)V"
+		),
+		require = 0
+	)
+	private Screen replaceScreen(Screen original) {
+		return new ConfigurationScreen(this);
+	}
+	#else
+	@Inject(
+		method = "method_19829",
+		at = @At("HEAD"),
+		allow = 1,
+		cancellable = true
+	)
+	private void wrapScreen(CallbackInfoReturnable<Screen> cir) {
+		if (!Config.injectUiComponents) {
+			return;
+		}
+		cir.returnValue = new ConfigurationScreen(this);
+	}
+	#endif
+}
